396ce02ac5ea328fb6fe24f30c25457c9bbcf416
권태호 <taehokwon48@gmail.com>
2021-09-23 09:50:00 +0900

    vue_fuck..

---
+++ test_project/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <header>
+      <ul>
+        <li><span>decideCnt</span></li>
+        <li><span>clearCnt</span></li>
+        <li><span>deathCnt</span></li>
+        <li><span>examCnt</span></li>
+      </ul>
+
+    </header>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+*{margin: 0 auto;}
+
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+main {
+  text-align: center;
+  margin-top: 40px;
+}
+ul{
+  display: flex;
+  justify-content: space-between;
+}
+header {
+  margin: 0px;
+  height: 70px;
+  padding: 0 16px 0 24px;
+  background-color: #35495E;
+  color: #ffffff;
+}
+
+header span {
+  display: block;
+  position: relative;
+  font-size: 20px;
+  line-height: 1;
+  letter-spacing: .02em;
+  font-weight: 400;
+  box-sizing: border-box;
+  padding-top: 24px;
+}
+</style>
---
+++ test_project/src/components/axios_test.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ cnt_test }}</h1>
+ 
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      cnt_test: []
+    }
+  },
+  created() {
+    	const APIKEY = "dfGEh0ZxCmcCs9D3T70URlxTe77aM%2FAVq%2F47cZb2gRko%2BxANXozELIoiMtAp92ANXucyBoEzbHvcMOYSXzzgDw%3D%3D";
+	const dataget = axios({
+	  method : 'get',
+	  url : 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson', 
+		params: {
+		  serviceKey: decodeURIComponent(APIKEY),
+		  pageNo: 1,
+		  numOfRows: 10,
+		  startCreateDt: 20210916,
+		  endCreateDt: 20210916
+		}
+	}).then(r => {
+	  
+	let a = JSON.stringify(r.data.response.body.items.item.decideCnt);
+	let b = JSON.stringify(r.data.response.body.items.item.clearCnt);
+	let c = JSON.stringify(r.data.response.body.items.item.deathCnt);
+	let d = JSON.stringify(r.data.response.body.items.item.examCnt);
+
+  this.cnt_test = response.data.map(a)
+	});
+	
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
